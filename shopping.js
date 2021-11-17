c72a612d0fe23a9585f9418a2d5665fea384a5db
Ali Paradise <alison.paradise@gmail.com>
2021-11-17 15:51:43 -0600

    array of objects for grocery list

---
+++ shopping.js
@@ -0,0 +1,58 @@
+const groceryList = [
+{  
+   id: 1,
+   name: "milk",
+   type: "whole",
+   count: 1,
+   flavor: "plain",
+   price: 3.99,
+   organic: true 
+},
+{
+    id: 2,
+    name: "eggs",
+    type: "chicken",
+    count: "1 dozen",
+    flavor: "brown",
+    price: 2.99,
+    organic: false
+},
+{
+    id: 3,
+    name: "bread",
+    type: "loaf",
+    count: 2,
+    flavor: "whole wheat",
+    price: 1.99,
+    organic: false
+},
+{
+    id: 4,
+    name: "apples",
+    type: "Gala",
+    count: 5,
+    flavor: "red",
+    price: 2.99,
+    organic: true
+},
+{
+    id: 5,
+    name: "lunchmeat",
+    type: "thin slice",
+    count: "1 pound",
+    flavor: "ham",
+    price: 4.99,
+    organic: false
+},
+{
+    id: 6,
+    name: "mustard",
+    type: "Heinz",
+    count: 1,
+    flavor: "spicy",
+    price: 2.99,
+    organic: false
+}
+]
+
+console.log(groceryList)
